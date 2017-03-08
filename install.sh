fdc70030ed57009998fb799cadc33ff9f5d7b51f
Robin Ole Heinemann <robin.ole.heinemann@t-online.de>
2017-03-08 12:51:50 +0100

    fix install

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #FILES=$(find . -maxdepth 1 -mindepth 1 -name '.*')
-for f in ""
+for f in ".clang-format .emacs.d .i3 .i3blocks.conf prompt.sh .tmux .tmux.conf tmuxline.conf .toprc .vim .vimrc .xinitrc .xprofile .Xresources .ycm_extra_conf.py .zshrc .config/nvim"
 do
-ln -s $(readlink -f $f) ~
+ln -s $(readlink -f $f) ~/$f
 done
